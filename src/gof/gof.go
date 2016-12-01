e11057e4d18d4c8d7ae97cf881e3a581dcc8031d
Robert Kánia <rk@redb.cz>
2016-12-01 15:35:33 +0100

    Json parametr z radku a vzorovy soubor

--- src/gof/gof.go
+++ src/gof/gof.go
@@ -3,49 +3,53 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"text/tabwriter"
 	"time"
-)
-
-const (
-	filename string = "./smlouvy.json"
 )
 
 func main() {
 	var y []byte
 	var f, faktury fakturace
 	var err error
-	var xml string
+	var xml, filename string
 	var now time.Time
 
-	if len(os.Args) == 2 {
-		if now, err = time.Parse(dateFormat, os.Args[1]); err != nil {
-			panic(err)
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Printf("Usage: %s input.json [1.12.2016] > output.xml\n\nWhere 1.12.2016 is invoice date. Otherwise, current date is used.\n\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	filename = os.Args[1]
+
+	if len(os.Args) == 3 {
+		if now, err = time.Parse(dateFormat, os.Args[2]); err != nil {
+			log.Fatal(err)
 		}
 	} else {
 		now = time.Now()
 	}
 
 	if y, err = ioutil.ReadFile(filename); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	if f, err = NewFakturace(y); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	if faktury, err = f.Fakturuj(now); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	if xml, err = faktury.XML(); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Print(xml)
-	if err = f.Save(filename + ".new"); err != nil {
-		panic(err)
+	if err = f.Save(filename); err != nil {
+		log.Fatal(err)
 	}
 
 	w := tabwriter.NewWriter(os.Stderr, 40, 8, 1, ' ', tabwriter.AlignRight)
